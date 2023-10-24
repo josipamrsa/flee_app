4612c7d9de924ab57d8633d135d6c3e0d3034a1d
Josipa Mrsa <josipa.mrsa.16@gmail.com>
2023-10-24 12:57:36 +0200

    added boilerplate screens for the rest of the app

--- app/src/main/java/com/example/fleeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/fleeapp/presentation/MainActivity.kt
@@ -3,13 +3,9 @@ package com.example.fleeapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.fleeapp.presentation.base_ui.FleeHeader
-import com.example.fleeapp.presentation.base_ui.theme.FleeAppTheme
 import com.example.fleeapp.presentation.base_ui.theme.flee_main.FleeMainTheme
 import com.example.fleeapp.presentation.navigation.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
---
+++ app/src/main/java/com/example/fleeapp/presentation/playlist_manager/PlaylistManagerScreen.kt
@@ -0,0 +1,31 @@
+package com.example.fleeapp.presentation.playlist_manager
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.fleeapp.presentation.base_ui.theme.flee_main.FleeMainTheme
+
+@Composable
+fun PlaylistManagerScreen(
+    navController: NavController
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(FleeMainTheme.colors.backgroundPrimary)
+    ) {
+        Text(
+            text = "Playlist manager screen",
+            color = FleeMainTheme.colors.textPrimary,
+        )
+    }
+}
